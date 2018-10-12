86d2871cf86d3264d47511722ba59f87ed0b296f
Heinki <Heinki@hotmail.de>
2018-10-13 00:48:27 +0200

    Add Whack a mole prototype

---
+++ Beansjam2018/Assets/_Project/0_Scripts/WackScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WackScript : MonoBehaviour {
+
+    Ray ray;
+    RaycastHit hit;    
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        touched();
+	}
+
+    void touched() {
+        for (int i = 0; i < Input.touchCount; i++) {
+            ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "Wack")
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+      
+    }
+}
