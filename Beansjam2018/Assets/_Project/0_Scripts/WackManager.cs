945cf1a09d94650fa693d8efe977fd74d30656dc
Heinki <Heinki@hotmail.de>
2018-10-13 11:40:27 +0200

    Add working Whack a mole prototype

---
+++ Beansjam2018/Assets/_Project/0_Scripts/WackManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WackManager : MonoBehaviour {
+
+    Ray ray;
+    RaycastHit hit;
+    GameObject[] wackables;
+    bool finished = false;
+    int hitItems;
+
+    // Use this for initialization
+    void Start () {
+        wackables = GameObject.FindGameObjectsWithTag("Wack");
+        hitItems = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        clicked();
+        touched();
+	}
+
+    void touched() {
+        for (int i = 0; i < Input.touchCount; i++) {
+            ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "Wack")
+                {
+                    GameObject wacked = hit.transform.gameObject;
+                    WackObject wackedObject = wacked.GetComponent<WackObject>();
+
+                    if (!wackedObject.getTouched())
+                    {
+                        wackedObject.setTouched(true);
+                        hitItems++;
+
+                        if (hitItems == wackables.Length)
+                        {
+                            this.finished = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void clicked()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "Wack")
+            {
+                GameObject wacked = hit.transform.gameObject;
+                WackObject wackedObject = wacked.GetComponent<WackObject>();
+
+                if (!wackedObject.getTouched())
+                {
+                    wackedObject.setTouched(true);
+                    hitItems++;
+
+                    if (hitItems == wackables.Length)
+                    {
+                        this.finished = true;
+                    }
+                }
+
+            }
+        }
+    }
+}
