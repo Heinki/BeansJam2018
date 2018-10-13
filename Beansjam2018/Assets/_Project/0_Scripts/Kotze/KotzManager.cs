b551009df0d6e0a222edc79ed87937866239e38f
unknown <pascal.worreschk@gmail.com>
2018-10-13 11:57:25 +0200

    Folder Sprites Added

---
+++ Beansjam2018/Assets/_Project/0_Scripts/Kotze/KotzManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KotzManager : MonoBehaviour {
+
+    
+    private bool gameSolved = false;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "Kotze")
+                {
+                    listKotze.Remove(hit.transform.gameObject);
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+        if(listKotze.Count == 0)
+        {
+            gameSolved = true;
+        }
+    }
+}
