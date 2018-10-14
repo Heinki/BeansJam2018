4d170fc6e1c948b31b0a7b509a37dcf863c7acd0
unknown <pascal.worreschk@gmail.com>
2018-10-14 14:22:49 +0200

    SoundManager Prefab implemented

---
+++ Beansjam2018/Assets/_Project/0_Scripts/Audio/AudioTester.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTester : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            SoundManager.instance.PlayRandomSFX_AUA();
+        } else if (Input.GetKeyDown(KeyCode.I))
+        {
+            SoundManager.instance.PlayRandomSFX_KOTZE();
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            SoundManager.instance.PlayRandomSFX_WELCOME();
+        }
+    }
+}
